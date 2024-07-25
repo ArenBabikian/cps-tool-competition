eeb12dfa6cb6cd1314fe154de8ff6002901f17f3
ArenBabikian <aren.babikian@mail.mcgill.ca>
2024-07-25 16:55:05 -0400

    Add other, previous tools

---
+++ optangle/_run/runFrenetic.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+current_date_time=$(date '+%d-%H-%M-%S')
+
+python competition.py \
+    --time-budget 100 \
+    --executor beamng \
+    --dave2-model "C:\git\cps-tool-competition\dave2\beamng-dave2.h5" \
+    --beamng-home "C:\applications\BeamNG.tech.v0.26.2.0" \
+    --beamng-user "C:\applications\BeamNG.tech.v0.26.2.0_userpath" \
+    --map-size 200 \
+    --speed-limit 70 \
+    --oob-tolerance 0.70 \
+    --log-to "C:\git\cps-tool-competition\aren\log\\${current_date_time}.log" \
+    --visualize-tests \
+    --module-path frenetic-sbst2021 \
+    --module-name src.generators.random_frenet_generator \
+    --class-name Frenetic
